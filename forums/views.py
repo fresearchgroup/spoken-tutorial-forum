c1285d6c7f1f72f999e8f25e71bad031a159facf
Saurabh Kumar <saurabh@saurabh-kumar.com>
2018-03-22 11:45:01 +0530

    remove /forgot-password/ url and view

--- forums/views.py
+++ forums/views.py
@@ -39,44 +39,6 @@ def user_logout(request):
     return HttpResponseRedirect('/')
 
 
-def forgotpassword(request):
-    context = {}
-    user_emails = []
-    context.update(csrf(request))
-    if request.method == 'POST':
-        users = User.objects.all()
-        for user in users:
-            user_emails.append(user.email)
-        email = request.POST['email']
-        if email == "":
-            context['invalid_email'] = True
-            return render_to_response("forums/templates/forgot-password.html", context)
-        if email in user_emails:
-            user = User.objects.get(email=email)
-            password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-            user.set_password(password)
-            user.save()
-            sender_name = "Spoken Forums"
-            sender_email = "no-reply@spoken-tutorial.org"
-            subject = "Spoken Forums - Password Reset"
-            to = (user.email, )
-            url = settings.EMAIL_URL
-            message = """Dear """ + user.username + """,\nYour password for Spoken Forums has been reset. Your credentials are:\nUsername: """ + user.username + """\nPassword: """ + password + \
-                """\n\nWe recommend you to login with the given credentials & update your password immediately.\nLink to set new password: """ + \
-                url + """/accounts/login/?next=/accounts/update-password/\nThank You !\nRegards,\nSpoken Team,\n IIT Bombay."""
-            send_mail(subject, message, sender_email, to)
-            form = UserLoginForm()
-            context['form'] = form
-            #context['password_reset'] = True
-            return HttpResponseRedirect('/accounts/login/?next=/accounts/update-password/')
-            # return render_to_response("forums/templates/user-login.html", context)
-        else:
-            context['invalid_email'] = True
-            return render_to_response("forums/templates/forgot-password.html", context)
-    else:
-        return render_to_response('forums/templates/forgot-password.html', context)
-
-
 def updatepassword(request):
     context = {}
     user = request.user
